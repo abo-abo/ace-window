e7e8ee216a81e7acafa500b08d695817d995dcab
Oleh Krehel <ohwoeowho@gmail.com>
2015-03-22 17:38:13 +0100

    ace-window.el: Update comments

--- ace-window.el
+++ ace-window.el
@@ -26,7 +26,7 @@
 ;;; Commentary:
 ;;
 ;; The main function, `ace-window' is meant to replace `other-window'.
-;; If fact, when there are only two windows present, `other-window' is
+;; In fact, when there are only two windows present, `other-window' is
 ;; called.  If there are more, each window will have its first
 ;; character highlighted.  Pressing that character will switch to that
 ;; window.
@@ -41,13 +41,16 @@
 ;;
 ;;    (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
 ;;
-;; This way they're all on the home row, although the intuitive
+;; This way they are all on the home row, although the intuitive
 ;; ordering is lost.
 ;;
 ;; If you don't want the gray background that makes the red selection
 ;; characters stand out more, set this:
 ;;
 ;;    (setq aw-background nil)
+;;
+;; If you want to know the selection characters ahead of time, you can
+;; turn on `ace-window-display-mode'.
 ;;
 ;; When prefixed with one `universal-argument', instead of switching
 ;; to selected window, the selected window is swapped with current one.
