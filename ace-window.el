e445b2c29b0eb1fa2231b6fa3d141bbc3ac752bf
Oleh Krehel <ohwoeowho@gmail.com>
2020-03-04 14:44:02 +0100

    ace-window.el (aw-select): Disable transient-mark-mode for the duration
    Fixes #188

--- ace-window.el
+++ ace-window.el
@@ -570,6 +570,7 @@ Amend MODE-LINE to the mode line for the duration of the selection."
                    (unwind-protect
                         (let* ((avy-handler-function aw-dispatch-function)
                                (avy-translate-char-function aw-translate-char-function)
+                               (transient-mark-mode nil)
                                (res (avy-read (avy-tree candidate-list aw-keys)
                                               (if (and ace-window-display-mode
                                                        (null aw-display-mode-overlay))
